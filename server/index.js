1bb3b1d20e6a388002ab6aa4dd3275946fd6131d
carsonharrell2 <thecarsonharrell@gmail.com>
2022-10-09 01:50:41 -0400

    init

---
+++ server/index.js
@@ -0,0 +1,15 @@
+// just to initialize the backend for now
+
+const express = require("express");
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.get("/api", (req, res) => {
+    res.json({ message: "Server response" });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server listening on ${PORT}`);
+});
